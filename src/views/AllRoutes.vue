06229098fb601dcc4628b50b4f2e6b3e209e04ae
richardleoverr <richard.leover@gmail.com>
2025-01-11 12:26:24 +0200

    Task 1

--- src/views/AllRoutes.vue
+++ src/views/AllRoutes.vue
@@ -3,34 +3,27 @@
 <h3> All Route  </h3>
 
 <!-- Task 1 -->
-    <div class="container">
-          <table>
-          <thead>
-          <tr>
-            <th>From</th>
-            <th>To</th>
-            <th>Cost</th>
-            <th>Time</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="item" v-for="route in routes" :key="route.id">
-            <td>{{ route.fromcity }} </td>
-            <td>{{ route.tocity}} </td>
-            <!-- <td> {{ route.cost}}</td> -->
-
-            <td class='red' v-if="route.cost > 12"> {{ route.cost}}</td>
-            <td class='blue' v-else>{{ route.cost}}</td>
-
-            <td>{{ route.departuretime }} </td>
-            <td>{{ route.departuredate }} </td> 
+    <div class = 'container'>
+      <table>    
+      <thead>
+      <tr>
+        <th>From</th>
+        <th>To</th>
+        <th>Cost</th>
+        <th>Time</th>
+        <th>Date</th>
+      </tr>
+          </thead>
+          <tbody>
+            <tr class = "item" v-for = "route in routes" :key="route.id">
+            <td>{{ route.fromcity }}</td>
+            <td>{{ route.tocity }}</td>
+            <td>{{ route.cost}}</td>
+            <td>{{ route.departuretime}}</td>
+            <td>{{ route.departuredate }}</td>
           </tr>
           </tbody>
-          </table>
-    </div>
-    <div class ='counters'  >
-            <p> We have  {{ routes.length }} trips today! </p>
+      </table>
     </div>
   </div>
 </template>
@@ -62,42 +55,7 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
-}
-th {
-  background: rgb(100, 151, 122);
-}
-td {
-  background: rgb(186, 228, 204);
-}
-th, td {
-  font-size: 15px;
-  margin-bottom: 5px;
-  padding: 8px 10px;
-}
-.red{
-  background: rgb(225, 33, 19); 
-}
-.blue{
-  background: rgb(19, 67, 163); 
-}
-.counters{
-    background: rgb(157, 160, 165); 
-    padding: 10px 20px;
-    display: block;
-    width: 40%;
-    margin: auto;
-    font-size: 18px;
-}
-.container {
-  background: #d5d7d8;
-  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
-  margin-bottom: 30px;
-  padding: 10px 20px;
-  margin: auto;
-  width: 40%;
-  display: flex;
-  justify-content: center;
-}
+
+
+
 </style>
